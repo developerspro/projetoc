57e9652547e7b1c56725097d1016439dae0e3355
Jeferson Ferreira <Jeferson.oferreira@etec.sp.gov.br>
2023-05-24 17:57:19 -0300

    alterações

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+// comentario de linha
+
+/*
+comentario de bloco
+*/
 main() {
-	
-printf(" Boa tarde! versao 1\nBoa tarde! versao 2");
+
+int numero1;
+int numero2;
+
+numero1 = 10;
+numero2 = 20;
+
+printf("Digite um valor para o numero 1: ");
+scanf("%d",&numero1);
+printf("O numero 1: %d\nE o numero 2: %d",numero1,numero2);
 
 }
